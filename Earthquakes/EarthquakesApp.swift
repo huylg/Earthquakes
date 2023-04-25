cceea9da032c8a0ba54c982906a335bc916d723f
huylg <lygiahuy11111998.hcmus@gmail.com>
2023-04-25 19:48:52 +0700

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Ly Gia Huy on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
