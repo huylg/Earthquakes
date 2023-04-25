6cfae5e00ca723ec8211597eee4add093904b7fe
huylg <lygiahuy11111998.hcmus@gmail.com>
2023-04-25 22:58:19 +0700

    update

---
+++ Earthquakes/Quake.swift
@@ -0,0 +1,50 @@
+//
+//  Quake.swift
+//  Earthquakes
+//
+//  Created by Ly Gia Huy on 25/04/2023.
+//
+
+import Foundation
+
+struct Quake {
+    let magnitude: Double
+    let place: String
+    let time: Date
+    let code: String
+    let detail: URL
+}
+
+extension Quake: Identifiable {
+    var id: String { code }
+}
+
+extension Quake: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case magnitude = "mag"
+        case place
+        case time
+        case code
+        case detail
+    }
+
+    init(from deconder: Decoder) throws {
+        let values = try deconder.container(keyedBy: CodingKeys.self)
+        let magnitude = try? values.decode(Double.self, forKey: .magnitude)
+        let place = try? values.decode(String.self, forKey: .place)
+        let time = try? values.decode(Date.self, forKey: .time)
+        let code = try? values.decode(String.self, forKey: .code)
+        let detail = try? values.decode(URL.self, forKey: .detail)
+
+        guard let magnitude,
+              let place,
+              let time,
+              let code,
+              let detail
+        else {
+            throw QuakeError.missingData
+        }
+
+        self.init(magnitude: magnitude, place: place, time: time, code: code, detail: detail)
+    }
+}
---
+++ Earthquakes/QuakeError.swift
@@ -0,0 +1,22 @@
+//
+//  QuakeError.swift
+//  Earthquakes
+//
+//  Created by Ly Gia Huy on 25/04/2023.
+//
+
+import Foundation
+
+enum QuakeError: Error {
+    case missingData
+}
+
+extension QuakeError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .missingData:
+            return NSLocalizedString("Found and will discard a quake missing a valid code, magnitude, plae, or time.",
+                                     comment: "")
+        }
+    }
+}
